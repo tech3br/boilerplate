7b8f2304c988606edcaae5efe6000f0c0996bccd
Daniel Filgueira <dfsdireito95@gmail.com>
2020-07-26 22:51:56 -0300

    add changes in gitignore, config of storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+    title = 'React Avancado',
+    description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => (
     <S.Wrapper>
         <S.Logo
             src="/img/reactjs-icon.svg"
             alt="Imagem de um atomo e React Avançado escrito ao lado"
         ></S.Logo>
-        <S.Title>React Avançado</S.Title>
-        <S.Description>
-            TypeScript, ReactJS, NextJS e Styled Components
-        </S.Description>
+        <S.Title>{title}</S.Title>
+        <S.Description>{description}</S.Description>
         <S.Ilustration
             src="/img/progressive_app.svg"
             alt="desenvolvedor de frente para tela com codigo"
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,20 +1,9 @@
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import Main from '.'
 
 describe('<Main />', () => {
     it('should render the heading', () => {
         render(<Main />)
-
-        expect(
-            screen.getByRole('heading', { name: /react avançado/i })
-        ).toBeInTheDocument()
-    })
-    it('should render the colors correctly', () => {
-        const { container } = render(<Main />)
-
-        expect(container.firstChild).toHaveStyle({
-            'background-color': '#06092b'
-        })
     })
 })
